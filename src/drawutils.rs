6b7dd8d4d9dc23247bc59222634658cd50235f27
Kyuri <toto4tot@math4totss-iMac.local>
2021-04-13 11:36:05 -0700

    fixed drawutils

--- src/drawutils.rs
+++ src/drawutils.rs
@@ -25,11 +25,14 @@ impl Canvas {
     pub fn set(&mut self, x: u32, y0: u32, color: Vec<f64>) {
         let flipped = true;
         let y = if flipped {self.height - 1 - y0} else {y0};
-        let r = (color[0] * 256.0) as u8;
-        let g = (color[1] * 256.0) as u8;
-        let b = (color[2] * 256.0) as u8;
-        println!("xy = {} {} rgb = {}, {}, {} Color = {:?}", x, y, r, g, b, color);
-        self.image.put_pixel(x, y, Rgb([r, g, b]));
+        if y < self.height {
+            let r = (color[0] * 256.0) as u8;
+            let g = (color[1] * 256.0) as u8;
+            let b = (color[2] * 256.0) as u8;
+            println!("xy = {} {} rgb = {}, {}, {} Color = {:?}", x, y, r, g, b, color);
+            self.image.put_pixel(x, y, Rgb([r, g, b]));
+        }
+
     }
 
     pub fn save(&self, name: &str) {
