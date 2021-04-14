7fdcd5c5612d05f328e0b7fd11288f5b5d1a808d
Kyuri <toto4tot@math4totss-iMac.local>
2021-04-14 11:38:32 -0700

    added hittable

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod tests {
     pub mod sect02;
     pub mod sect03;
     pub mod sect04;
+    pub mod sect06;
 }
 pub mod vec3;
 pub mod drawutils;
 pub mod ray;
+pub mod hittable;
 
 fn main() {
     println!("Hello, world!");
