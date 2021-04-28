43d19937121cccd298df578ee3ed784026faa6bc
Kyuri <toto4tot@math4totss-iMac.local>
2021-04-28 11:06:00 -0700

    orienting

--- src/camera.rs
+++ src/camera.rs
@@ -13,31 +13,35 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(lookfrom: Point, lookat: Point, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
         let theta = degrees_to_radians(vfov);
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
-        let viewport_width = aspect_ratio * viewport_height;
-        
-        let focal_length = 1.0;
+        let viewport_width = aspect_ratio * viewport_height;        
+
+        let w = (lookfrom - lookat).unit_vector();
+        let u = (vup.cross(w)).unit_vector();
+        let v = w.cross(u);
+
+
         Camera {
-            origin: Point::new(0, 0, 0),
-            horizontal: Vec3::new(viewport_width, 0.0, 0.0),
-            vertical: Vec3::new(0.0, viewport_height, 0.0),
+            origin: lookfrom,
+            horizontal: u.scale(viewport_width),
+            vertical: v.scale(viewport_height),
             lower_left_corner: 
-                            Point::new(0, 0, 0) - 
-                            Vec3::new(viewport_width, 0.0, 0.0).scale(1.0 / 2.0) - 
-                            Vec3::new(0.0, viewport_height, 0.0).scale(1.0 / 2.0) - 
-                            Vec3::new(0.0, 0.0, focal_length)
+                            lookfrom - 
+                            (u.scale(viewport_width)).scale(1.0 / 2.0) - 
+                            (v.scale(viewport_height)).scale(1.0 / 2.0) - 
+                            w
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
             self.lower_left_corner + 
-            self.horizontal.scale(u) +
-            self.vertical.scale(v) - 
+            self.horizontal.scale(s) +
+            self.vertical.scale(t) - 
             self.origin
         )
     }
